8d49615203b7be91641a277364d10e2000568258
TheaAmo <tamoh060@uottawa.ca>
2025-11-26 15:48:57 -0500

    implement theo's feature: the cooking page is done both jsx file and css.

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './index.css';
 import HomePage from './pages/HomePage.jsx';
 import AuthPage from './pages/AuthPage.jsx'; // make sure this path is correct
 import MorePage from './pages/MorePage.jsx';
+import CookingPage from './pages/CookingPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<AuthPage />} />
         <Route path="/more" element={<MorePage/>} />
+        <Route path="/cooking" element={<CookingPage/>} />
       </Routes>
     </Router>
   </StrictMode>
